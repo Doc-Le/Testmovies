86b22604ed2e82b9c59c114c27faed0d5cce4740
Doc-Le <leandro.arj@outlook.com>
2022-04-06 20:16:21 +0000

    Create flask app

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("MOVIE_API_KEY", "167e549351344df34d25699f6fba4e30")
+os.environ.setdefault("MONGO_URI", "")
+os.environ.setdefault("MONGO_DBNAME", "testmoviestest")
